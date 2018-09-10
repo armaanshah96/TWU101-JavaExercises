6023a98ebd99029a71c1327f859f6f8a5b7e057c
Armaan Shah <armaanshah96@gmail.com>
2018-09-10 17:58:44 -0400

    setup commit

---
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise1/Main.java
@@ -0,0 +1,15 @@
+package com.thoughtworks.tw101.exercises.exercise1;
+
+// Exercise #1: Print all odd numbers from 1 to 100 and their sum.
+
+public class Main {
+    public static void printOdds(int max) {
+        for(int i = 1; i < max; i+=2) {
+            System.out.print(i + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        printOdds(100);
+    }
+}
