94a8e618a9cbc120d9bcd9fee107495b74292d38
Armaan Shah <armaanshah96@gmail.com>
2018-09-10 21:01:03 -0400

    exercise 2

--- src/main/java/com/thoughtworks/tw101/exercises/exercise2/Accumulator.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise2/Accumulator.java
@@ -1,4 +1,17 @@
 package com.thoughtworks.tw101.exercises.exercise2;
 
 public class Accumulator {
+    int acc;
+
+    public Accumulator() {
+        acc = 0;
+    }
+
+    void increment() {
+        acc++;
+    }
+
+    void total() {
+        System.out.println(acc);
+    }
 }
